0b9193341bcab4c4effa49f5c26e85a90036aec9
felipezoltowski <felipe.zoltowski@gmail.com>
2022-11-18 11:23:54 -0300

    Creating user token

---
+++ src/modules/accounts/useCases/authenticationUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        // User existe
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new Error("Email or password incorrect!");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+        // Senha correta
+        if (!passwordMatch) {
+            throw new Error("Email or password incorrect!");
+        }
+
+        const token = sign({}, "68ed38dc744a3eaa8efd5dfb2848b6e0", {
+            subject: user.id,
+            expiresIn: "1d",
+        });
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+            },
+        };
+
+        return tokenReturn;
+        // Gerar jsonwebtoken
+    }
+}
+
+export { AuthenticateUserUseCase };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationRoutes } from "./specificatons.routes";
 import { usersRoutes } from "./users.routes";
@@ -9,5 +10,6 @@ const router = Router();
 router.use("/users", usersRoutes);
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationRoutes);
+router.use(authenticateRoutes);
 
 export { router };
