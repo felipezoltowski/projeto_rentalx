ed1f34031a5474eaa26127be4bb7a603cf07d183
felipezoltowski <felipe.zoltowski@gmail.com>
2023-10-24 20:49:37 -0300

    create admin seed

--- src/shared/infra/http/routes/specificatons.routes.ts
+++ src/shared/infra/http/routes/specificatons.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-import { CreateSpecificationController } from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
+
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 
 const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationRoutes.use(ensureAuthenticated);
-specificationRoutes.post("/", createSpecificationController.handle);
+specificationRoutes.post("/", ensureAuthenticated, ensureAdmin, createSpecificationController.handle);
 
 export { specificationRoutes };
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,14 +1,24 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 interface IOptions {
     host: string;
 }
 
-getConnectionOptions().then((options) => {
-    const newOptions = options as IOptions;
-    newOptions.host = "database"; // Nome dado ao service do banco de dados
+// getConnectionOptions().then((options) => {
+//     const newOptions = options as IOptions;
+//     newOptions.host = "database"; // Nome dado ao service do banco de dados
 
-    createConnection({
-        ...options,
-    });
-});
+//     createConnection({
+//         ...options,
+//     });
+// });
+
+export default async(host = "database"): Promise<Connection> => {
+    const defaultOptions = await getConnectionOptions();
+
+    return createConnection(
+        Object.assign(defaultOptions, {
+            host,
+        })
+    )
+}
